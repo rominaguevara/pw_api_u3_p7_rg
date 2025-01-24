cfadc7c3ac0637b553e1c71bebfc713e417e6b6e
Romina Guevara <rominaguevara2207@hotmail.com>
2025-01-23 19:50:56 -0500

    Taller25

---
+++ src/test/java/uce/edu/web/api/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package uce.edu.web.api;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}

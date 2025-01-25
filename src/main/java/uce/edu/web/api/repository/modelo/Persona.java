99373fc76006e20385480c46d2ec4952e2e000e2
Romina Guevara <rominaguevara2207@hotmail.com>
2025-01-24 19:48:08 -0500

    Taller26

---
+++ src/main/java/uce/edu/web/api/repository/modelo/Persona.java
@@ -0,0 +1,63 @@
+package uce.edu.web.api.repository.modelo;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
+@Table(name = "persona")
+public class Persona {
+
+    @Id
+    @GeneratedValue(generator = "seq_persona", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "seq_persona", sequenceName = "seq_persona", allocationSize = 1)
+    @Column(name = "pers_id")
+    private Integer id;
+
+    @Column(name = "pers_nombre")
+    private String nombre;
+
+    @Column(name = "pers_apellido")
+    private String apellido;
+
+    @Column(name = "pers_fecha_nacimiento")
+    private LocalDateTime fechaNacimiento;
+
+
+    //SET y GET
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public LocalDateTime getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    
+    
+}

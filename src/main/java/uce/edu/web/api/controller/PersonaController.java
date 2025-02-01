7fe7a68d9bc85ed6448d472ff0cc314802d80a0e
Romina Guevara <rominaguevara2207@hotmail.com>
2025-01-31 19:47:50 -0500

    Taller29

--- src/main/java/uce/edu/web/api/controller/PersonaController.java
+++ src/main/java/uce/edu/web/api/controller/PersonaController.java
@@ -1,9 +1,12 @@
 package uce.edu.web.api.controller;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.service.IPersonaService;
 import uce.edu.web.api.service.to.PersonaTo;
 
@@ -13,21 +16,36 @@ public class PersonaController {
     private IPersonaService iPersonaService;
     @GET
     @Path("/buscar")
-    public Response buscarPorId() {
+    public PersonaTo buscarPorId() {
         Integer id=1;
-        //return this.iPersonaService.buscarPorId(id);
-        return Response.ok(this.iPersonaService.buscarPorId(id)).build();
+        return this.iPersonaService.buscarPorId(id);
+        //return Response.ok(this.iPersonaService.buscarPorId(id)).build();
     }
     
+    @POST
+    @Path("/guardar")
     public void guardar(PersonaTo persona) {
         this.iPersonaService.guardar(persona);
     }
     
+    @PUT
+    @Path("/actualizar")
     public void actualizar(PersonaTo persona) {
         this.iPersonaService.actualizar(persona);
     }
 
-    public void borrar(Integer id) {
+    @PATCH
+    @Path("/actualizar/parcial")
+    public void actualizarParcial(PersonaTo persona) {
+        PersonaTo tmp = this.iPersonaService.buscarPorId(persona.getId());
+        tmp.setNombre(persona.getNombre());
+        this.iPersonaService.actualizar(tmp);
+    }
+
+    @DELETE
+    @Path("/borrar")
+    public void borrar() {
+        Integer id = 1;
         this.iPersonaService.borrar(id);
     }
 }

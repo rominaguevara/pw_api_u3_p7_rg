5ac22a1031b63933ac597b3adbdbafc12b84092b
Romina Guevara <rominaguevara2207@hotmail.com>
2025-02-05 19:50:47 -0500

    Taller30

--- src/main/java/uce/edu/web/api/controller/PersonaController.java
+++ src/main/java/uce/edu/web/api/controller/PersonaController.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.PathParam;
 import uce.edu.web.api.service.IPersonaService;
 import uce.edu.web.api.service.to.PersonaTo;
 
@@ -17,37 +17,36 @@ public class PersonaController {
     private IPersonaService iPersonaService;
    
     @GET
-    @Path("/buscar")
-    public PersonaTo buscarPorId() {
-        Integer id=1;
+    @Path("/{id}")
+    public PersonaTo buscarPorId(@PathParam("id") Integer id) {
         return this.iPersonaService.buscarPorId(id);
-        //return Response.ok(this.iPersonaService.buscarPorId(id)).build();
     }
     
     @POST
-    @Path("/guardar")
+    @Path("")
     public void guardar(PersonaTo persona) {
         this.iPersonaService.guardar(persona);
     }
     
     @PUT
-    @Path("/actualizar")
-    public void actualizar(PersonaTo persona) {
+    @Path("/{id}")
+    public void actualizar(PersonaTo persona,@PathParam("id") Integer id) {
+        persona.setId(id);
         this.iPersonaService.actualizar(persona);
     }
 
     @PATCH
-    @Path("/actualizar/parcial")
-    public void actualizarParcial(PersonaTo persona) {
-        PersonaTo tmp = this.iPersonaService.buscarPorId(persona.getId());
+    @Path("/{id}/nuevo/{cedula}")
+    public void actualizarParcial(PersonaTo persona, @PathParam("id")Integer id, @PathParam("cedula")String cedula) {
+        System.out.println(cedula);
+        PersonaTo tmp = this.iPersonaService.buscarPorId(id);
         tmp.setNombre(persona.getNombre());
         this.iPersonaService.actualizar(tmp);
     }
 
     @DELETE
-    @Path("/borrar")
-    public void borrar() {
-        Integer id = 1;
+    @Path("/{id}")
+    public void borrar(@PathParam("id") Integer id) {
         this.iPersonaService.borrar(id);
     }
 

d4f483cfae58ab279961d87d9097bbcb80d0930b
Romina Guevara <rominaguevara2207@hotmail.com>
2025-02-12 19:42:19 -0500

    Taller32

--- src/main/java/uce/edu/web/api/controller/PersonaController.java
+++ src/main/java/uce/edu/web/api/controller/PersonaController.java
@@ -10,7 +10,10 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.service.IPersonaService;
 import uce.edu.web.api.service.to.PersonaTo;
 
@@ -21,9 +24,12 @@ public class PersonaController {
    
     @GET
     @Path("/{id}")
-    public PersonaTo buscarPorId(@PathParam("id") Integer id) {
-        
-        return this.iPersonaService.buscarPorId(id);
+    @Produces(MediaType.APPLICATION_XML)
+    public Response buscarPorId(@PathParam("id") Integer id) {
+        return Response.status(240)
+        .header("mensaje", "Persona creada pero en proceso de validación")
+        .header("valor1", 500)
+        .entity(this.iPersonaService.buscarPorId(id)).build();        
     }
 
 
--- src/main/java/uce/edu/web/api/service/to/PersonaTo.java
+++ src/main/java/uce/edu/web/api/service/to/PersonaTo.java
@@ -3,6 +3,10 @@ package uce.edu.web.api.service.to;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class PersonaTo implements Serializable {
     private Integer id;
     private String nombre;
@@ -17,24 +21,31 @@ public class PersonaTo implements Serializable {
         this.apellido = apellido;
         this.fechaNacimiento = fechaNacimiento;
     }
+    @XmlElement
     public Integer getId() {
         return id;
     }
     public void setId(Integer id) {
         this.id = id;
     }
+
+    @XmlElement
     public String getNombre() {
         return nombre;
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    @XmlElement
     public String getApellido() {
         return apellido;
     }
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+
+    @XmlElement
     public LocalDateTime getFechaNacimiento() {
         return fechaNacimiento;
     }
